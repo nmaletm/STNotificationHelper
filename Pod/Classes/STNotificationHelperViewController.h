4f2de65ec351d8e7bebf644f7f11be6dc4d1a6d3
Nestor <nmaletm@gmail.com>
2014-10-19 12:01:56 +0200

    Initial commit

---
+++ Pod/Classes/STNotificationHelperViewController.h
@@ -0,0 +1,72 @@
+//
+//  STNotificationHelperViewController.h
+//
+//
+
+#import <UIKit/UIKit.h>
+
+extern NSBundle *STNotificationBundle(void);
+extern void STNotificationCustomLocalizationBlock(NSString *(^customLocalizationBlock)(NSString *stringToLocalize));
+extern NSString *STNotificationLocalizedString(NSString *localizeString);
+
+@interface STNotificationHelperObject : NSObject
+
+@property (nonatomic,strong) NSString *title;
+@property (nonatomic,strong) NSString *notifciationDescription;
+@property (nonatomic,strong) UIImage  *appIcon;
+@property (nonatomic,strong) NSString *appName;
+
+
+- (instancetype)initWithTitle:(NSString*)title
+                  description:(NSString*)notificationDescription
+                      appIcon:(UIImage*)appIcon
+                      appName:(NSString*)appname;
+
+
++ (instancetype)objectWithTitle:(NSString*)title
+                    description:(NSString*)notificationDescription
+                        appIcon:(UIImage*)appIcon
+                        appName:(NSString*)appname;
+@end
+
+@interface STUISwitchView : UIView
+@end
+
+
+
+@interface STNotificationHelperViewController : UIViewController
+@property (nonatomic,strong) UILabel        *titleLabel;
+@property (nonatomic,strong) UILabel        *descriptionLabel;
+
+@property (nonatomic,strong) UIButton       *exitButton;
+@property (nonatomic,strong) UILabel        *doesItWorkLabel;
+
+@property (nonatomic,strong) UILabel        *numberOneLabel;
+@property (nonatomic,strong) UILabel        *numberOneDescriptionLabel;
+@property (nonatomic,strong) UIImageView    *numberOneImageView;
+
+@property (nonatomic,strong) UILabel        *numberTwoLabel;
+@property (nonatomic,strong) UILabel        *numberTwoDescriptionLabel;
+@property (nonatomic,strong) UIImageView    *numberTwoImageView;
+
+@property (nonatomic,strong) UILabel        *numberThreeLabel;
+@property (nonatomic,strong) UILabel        *numberThreeDescriptionLabel;
+@property (nonatomic,strong) UIImageView    *numberThreeImageView;
+
+
+@property (nonatomic,strong) UILabel        *numberFourLabel;
+@property (nonatomic,strong) UILabel        *numberFourDescriptionLabel;
+@property (nonatomic,strong) UIView         *numberFourDescriptionView;
+@property (nonatomic,strong) STUISwitchView *numberFourSwitchView;
+@property (nonatomic,strong) UILabel        *numberFourDescriptionLabelInDescriptionView;
+
+
+@property (nonatomic,strong) UILabel        *numberFiveLabel;
+@property (nonatomic,strong) UILabel        *numberFiveDescriptionLabel;
+@property (nonatomic,strong) UIView         *numberFiveDescriptionView;
+@property (nonatomic,strong) UILabel        *noneLabel;
+@property (nonatomic,strong) UILabel        *bannerLabel;
+@property (nonatomic,strong) UILabel        *hinweisLabel;
+
+- (id)initWithNotification:(STNotificationHelperObject*)notification;
+@end
--- README.md
+++ README.md
@@ -1,28 +1,39 @@
 # STNotificationHelper
 
-[![CI Status](http://img.shields.io/travis/Nestor/STNotificationHelper.svg?style=flat)](https://travis-ci.org/Nestor/STNotificationHelper)
 [![Version](https://img.shields.io/cocoapods/v/STNotificationHelper.svg?style=flat)](http://cocoadocs.org/docsets/STNotificationHelper)
 [![License](https://img.shields.io/cocoapods/l/STNotificationHelper.svg?style=flat)](http://cocoadocs.org/docsets/STNotificationHelper)
 [![Platform](https://img.shields.io/cocoapods/p/STNotificationHelper.svg?style=flat)](http://cocoadocs.org/docsets/STNotificationHelper)
 
-## Usage
+## Podfile
 
-To run the example project, clone the repo, and run `pod install` from the Example directory first.
+```ruby
+platform :ios, '6.0'
+pod 'STNotificationHelper'
+```
 
-## Requirements
+##Usage
 
-## Installation
+```objective-c
+NSString *title = @"Benachrichtungen aktivieren";
+NSString *descriptionString = @"Um die Notificationen verwenden zu können müssen sie die Banachrichtungen aktivieren.";
 
-STNotificationHelper is available through [CocoaPods](http://cocoapods.org). To install
-it, simply add the following line to your Podfile:
+MHNotificationHelperObject *notificationObject = [MHNotificationHelperObject objectWithTitle:title
+description:descriptionString
+appIcon:nil
+appName:@"meine App"];
 
-    pod "STNotificationHelper"
+MHNotificationHelperViewController *notificationHelper = [MHNotificationHelperViewController.alloc initWithNotification:notificationObject];
+notificationHelper.bannerLabel.text = NSLocalizedString(@"Banner", nil);
 
-## Author
+[self presentViewController:notificationHelper animated:YES completion:nil];
 
-Nestor, nmaletm@gmail.com
+
+```
+## Authors
+
+Mario Hahn, https://github.com/mariohahn
+Nestor, https://github.com/nmaletm
 
 ## License
 
 STNotificationHelper is available under the MIT license. See the LICENSE file for more info.
-
